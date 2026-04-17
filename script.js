3a770edc31af16bcba0242a7a164942e3d2b7899
Stephanie Yu <stephy8@uci.edu>
2026-04-17 01:27:34 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
-console.log("Todo app");
+const input = document.getElementById("todo-input");
+const addButton = document.getElementById("add-btn");
+const todoList = document.getElementById("todo-list");
+
+addButton.addEventListener("click", function () {
+  const taskText = input.value.trim();
+
+  if (taskText === "") {
+    return;
+  }
+
+  const li = document.createElement("li");
+  li.textContent = taskText;
+
+  todoList.appendChild(li);
+  input.value = "";
+});
